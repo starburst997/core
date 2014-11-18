0b027b7d4a763610d1258fa9a950f9222506c349
Michael Baczynski <michaelbaczynski@gmail.com>
2014-11-18 17:42:16 +0100

    update Vec2 class

--- src/de/polygonal/core/math/Vec2.hx
+++ src/de/polygonal/core/math/Vec2.hx
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2012-2014 Michael Baczynski, http://www.polygonal.de
+Copyright (c) 2014 Michael Baczynski, http://www.polygonal.de
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -18,53 +18,196 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 package de.polygonal.core.math;
 
-import de.polygonal.core.math.Mathematics;
+import de.polygonal.core.math.Mathematics.M;
+import de.polygonal.core.math.random.Random;
 
 /**
- * <p>A 2D vector a geometric object that has both a magnitude (or length) and direction.</p>
- */
-class Vec2
+	A 2d vector.
+	
+	A geometric object that has both a magnitude (or length) and direction.
+**/
+class Vec2 extends Coord2f
 {
 	/**
-	 * The x-component.
-	 */
-	public var x:Float;
+		Returns the angle between segments formed by the vector `a` and `b`.
+	**/
+	inline public static function angle(a:Vec2, b:Vec2):Float
+	{
+		return Math.atan2(perpDot(a, b), dot(a, b));
+	}
 	
 	/**
-	 * The y-component.
+		Computes the unit length vector `out` = (`b`-`a`)/||`b`-`a`|| and returns the length of `b`-`a`.
+	**/
+	inline public static function dir(a:Vec2, b:Vec2, out:Vec2):Float
+	{
+		var dx = b.x - a.x;
+		var dy = b.y - a.y;
+		var l = Math.sqrt(dx * dx + dy * dy);
+		out.x = dx / l;
+		out.y = dy / l;
+		return l;
+	}
+	
+	/**
+		Computes the dot product `a`×`b`.
+		
+		Also known as inner product or scalar product.
+	**/
+	inline public static function dot(a:Vec2, b:Vec2):Float
+	{
+		return a.x * b.x + a.y * b.y;
+	}
+	
+	/**
+		Tests if `c` is left, on or right of an infinite line through `a` and `b`.
+		
+		Returns a value that is:
+		
+		 - &gt; 0 for `c` left of the line through `a` and `b`
+		 - = 0 for `c` on the line
+		 - &lt; 0 for `c` right of the line
+	**/
+	inline public static function isLeft(a:Vec2, b:Vec2, c:Vec2):Float
+	{
+		return (b.y - a.x) * (c.y - a.y) - (c.y - a.x) * (b.y - a.y);
+	}
+	
+	/**
+		Returns `out` = min(`a`,`b`).
+	**/
+	inline public static function min(a:Vec2, b:Vec2, out:Vec2)
+	{
+		out.x = M.fmin(a.x, b.x);
+		out.y = M.fmin(a.y, b.y);
+	}
+	
+	/**
+		Returns `out` = max(`a`,`b`).
+	**/
+	inline public static function max(a:Vec2, b:Vec2, out:Vec2)
+	{
+		out.x = M.fmax(a.x, b.x);
+		out.y = M.fmax(a.y, b.y);
+	}
+	
+	/**
+		Returns the midpoint `out` = (`a`+`b`)/2.
+	**/
+	inline public static function midpoint(a:Vec2, b:Vec2, out:Vec2):Vec2
+	{
+		out.x = a.x + (b.x - a.x) / 2;
+		out.y = a.y + (b.y - a.y) / 2;
+		return out;
+	}
+	
+	/**
+	 * Gram-Schmidt orthonormalization of `u` and `v`.
 	 */
-	public var y:Float;
-	
-	public function new(x = 0., y = 0.)
+    public static function orthonormalize(u:Vec2, v:Vec2)
 	{
-		this.x = x;
-		this.y = y;
+		u.normalize();
+		var uv = dot(u, v);
+		v.x -= u.x * uv;
+		v.y -= u.y * uv;
+		v.normalize();
 	}
 	
-	/** Set to zero vector. */
-	inline public function zero():Vec2
+	/**
+		Computes the perpendicular bisector `out` of the segment between the point `a` and `b`.
+		
+		The perpendicular bisector is the line that passes through the midpoint between `a` and `b`
+		(given by (`a`+`b`)/2) and is perpendicular to the vector `b`-`a`.
+		Equation: p(t) = 1/2 (`a`+`b`) + perp(`b`-`a`)`t`
+	**/
+	inline public static function perpBisecor(a:Vec2, b:Vec2, t:Float, out:Vec2)
 	{
-		x = y = 0;
-		return this;
+		var ax = a.x;
+		var ay = a.y;
+		var bx = b.x;
+		var by = b.y;
+		var dx = -(by - ay);
+		var dy =  (bx - ax);
+		out.x = (ax + bx) / 2 + dx * t;
+		out.y = (ay + by) / 2 + dy * t;
 	}
 	
-	/** Unary perp operator.<br/>Computes the normal vector pointing to the left (CCW) of this vector and returns this vector. */
-	inline public function perp():Vec2
+	/**
+		Computes the perp-dot product perp(`a`)`b`, where perp() is defined to rotate a vector 90° counterclockwise (CCW).
+		
+		Also known as exterior product or outer product.
+		
+		This is the determinant of the matrix with first row `a` and second row `b`.
+	**/
+	inline public static function perpDot(a:Vec2, b:Vec2):Float
+	{
+		return a.x * b.y - a.y * b.x;
+	}
+	
+	/**
+		Creates a random vector with x in the range [`minX`,`maxX`] and y in the range [`minY`,`maxY`].
+	**/
+	inline public static function random(minX:Float, maxX:Float, minY:Float, maxY:Float):Vec2
+	{
+		return new Vec2(Random.frandRange(minX, maxX), Random.frandRange(minY, maxY));
+	}
+	
+	/**
+		Vector reflection. Returns `out` = `v`-(2dot(`v`,`n`))`n`.
+	**/
+	inline public static function reflect(v:Vec2, n:Vec2, out:Vec2):Vec2
+	{
+		var t = v.x * n.x + v.y * n.y;
+		out.x = v.x - (2 * t) * n.x;
+		out.y = v.y - (2 * t) * n.y;
+		return out;
+	}
+	
+	/**
+		Computes the signed triangle area formed by the points `a`, `b` and `c`.
+	**/
+	inline public static function signedTriArea(a:Vec2, b:Vec2, c:Vec2):Float
+	{
+		return (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+	}
+	
+	/**
+		The vector length (aka norm).
+	**/
+	public var length(get_length, never):Float;
+	function get_length():Float return return Math.sqrt(squaredLength);
+	
+	/**
+		The squared vector length.
+	**/
+	public var squaredLength(get_squaredLength, never):Float;
+	inline function get_squaredLength():Float return x * x + y * y;
+	
+	public function new(x:Float = 0., y:Float = 0.)
+	{
+		super(x, y);
+	}
+	
+	/**
+	 * Right-handed perp operator (z-axis pointing out of the screen); returns (`y`, -`x`).
+	 */
+	inline public function perpR()
+	{
+		var t = y; y = -x; x = t;
+	}
+	
+	/**
+		Left-handed perp operator (z-axis pointing into the screen); returns (-`y`, `x`).
+	**/
+	inline public function perpL()
 	{
 		var t = y; y = x; x = -t;
-		return this;
 	}
 	
-	/** Flips and returns this vector. */
-	inline public function flip():Vec2
-	{
-		x = -x;
-		y = -y;
-		return this;
-	}
-	
-	/** Converts this vector to unit length and returns the original vector length. */
-	inline public function unit():Float
+	/**
+		Converts this vector to unit length and returns the original vector length.
+	**/
+	inline public function normalize():Float
 	{
 		var t = Math.sqrt(x * x + y * y);
 		x /= t;
@@ -72,57 +215,17 @@ class Vec2
 		return t;
 	}
 	
-	/** Computes the vector length (aka norm). */
-	inline public function length():Float
-	{
-		return Math.sqrt(x * x + y * y);
-	}
-	
-	/** Computes the inverse length using a  fast approximation. */
-	inline public function invLength():Float
-	{
-		return M.invSqrt(x * x + y * y);
-	}
-	
-	/** Computes the squared vector length. */
-	inline public function lengthSqr():Float
-	{
-		return x * x + y * y;
-	}
-	
-	/** Clamps this vector to <code>max</code> length. */
+	/**
+	 * Clamps this vector to <code>max</code> length.
+	 */
 	inline public function clamp(max:Float)
 	{
-		var tx = x;
-		var ty = y;
-		var len = tx * tx + ty * ty;
-		if (len > max * max)
+		var l = squaredLength;
+		if (l > max * max)
 		{
-			len = Math.sqrt(len);
-			x = (tx / len) * max;
-			y = (ty / len) * max;
+			l = Math.sqrt(l);
+			x = (x / l) * max;
+			y = (y / l) * max;
 		}
-	}
-	
-	/** Assigns the values of <code>other</code> to this. */
-	inline public function set(other:Vec2):Vec2
-	{
-		x = other.x;
-		y = other.y;
-		return this;
-	}
-	
-	/** Creates and returns a copy of this object. */
-	public function clone():Vec2
-	{
-		return new Vec2(x, y);
-	}
-	
-	/** Returns the string form of the value that this object represents. */
-	public function toString():String
-	{
-		return Printf.format("Vec2:\n" +
-			"[%-+10.4f]\n" +
-			"[%-+10.4f]", [x, y]);
 	}
 }
