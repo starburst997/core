7f906d1b3d4325595647c44bf4e776e91c2c20d6
Michael Baczynski <michaelbaczynski@gmail.com>
2016-12-04 14:12:11 +0100

    EntitySubject.addCallback()

--- src/de/polygonal/core/tools/FileSequence.hx
+++ src/de/polygonal/core/tools/FileSequence.hx
@@ -18,93 +18,201 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 */
 package de.polygonal.core.tools;
 
-import de.polygonal.ds.Sll;
-import de.polygonal.ds.tools.Compare;
+import haxe.ds.StringMap;
 
-//private typedef Strings = Array<String>;
-
+/**
+	Finds groups of items that follow a naming convention containing a numerical sequence index (e.g. fileA.001.png, fileA.002.png, fileA.003.png...).
+**/
 class FileSequence
 {
-	public static function find(values:Array<String>):Array<Array<String>>
+	var mTrailingDigitsOnly = false;
+	
+	public function new(trailingDigitsOnly = false)
 	{
-		var first = null;
-		var ereg:EReg = null;
-		
-		var match = function(a, b)
-		{
-			/*if (a != first)
-			{
-				first = a;
-				ereg = getRegEx(a);
-			}*/
-			//return ereg.match(b);
-			
-			return getRegEx(a).match(b);
-		}
-		
-		var r = ~/\d+/g;
-		
-		var matches = split(values, match);
-		
-		for (i in matches)
-		{
-			L.i('match list:');
-			trace(i.join("\n"));
-		}
-		
-		
-		var set = matches[1];
-		var x = set.map(function(e) return r.replace(e, ""));
-		
-		match = function(a, b) return r.replace(a, "") == r.replace(b, "");
-		var output = split(set, match);
-		
-		for (i in output)
-		{
-			L.w(i);
-			//000 W/activity(TestFileSequence.main 0092): [bla_1_04,bla_2_06,bla_3_05]
-			
-			//now we have a potential set that can be a sequence
-			
-			i.sort(Compare.cmpAlphabeticalRise);
-			
-			L.w(i);
-			//000 W/activity(TestFileSequence.main 0098): [bla_1_04,bla_2_06,bla_3_05]
-			
-			//TODO find i
-			
-			var sequenceList = test(i);
-		
-			for (i in sequenceList)
-			{
-				trace('sequence:');
-				for (j in i)
-				{
-					trace('  ' + j);
-				}
-			}
-			break;
-		}
-		
-		
-		return null;
+		mTrailingDigitsOnly = trailingDigitsOnly;
 	}
 	
-	static function getRegEx(string:String):EReg
+	public function find(values:Array<String>):Array<{name:String, items:Array<String>}>
 	{
-		//build regular expression; e.g. foo_001_bar1.ext -> "^.{4}\d{3}.{4}\d{1}.{4}$"
+		values = values.copy();
+		
+		var matchDigits = ~/\d+/g;
+		var output = [];
+		
+		discardStringsWithoutDigits(values);
+		if (values.length == 0) return output; //quit; nothing to do
+		
+		/* group by comparing regular expressions, e.g. "foo01bar001" is transformed to "\D{3}\d{2}\D{3}\d{3}" */
+		
+		var regexLut = new StringMap<EReg>();
+		for (i in values) regexLut.set(i, getRegEx(i));
+		var groups = assign(values, function(a, b) return regexLut.get(a).match(b));
+		
+		/* remove numbers; string portion has to be identical; e.g. "a1b" and "a2c" are transformed to "ab" and "ac" */
+		
+		var cmpAlphabetical = function(a:String, b:String)
+		{
+			a = a.toLowerCase();
+			b = b.toLowerCase();
+			return a < b ? -1 : (a > b ? 1 : 0);
+		}
+		var tmp = new Array<Array<String>>();
+		var stringLut:StringMap<String>;
+		var cmp = function(a:String, b:String):Bool return stringLut.get(a) == stringLut.get(b);
+		for (group in groups)
+		{
+			stringLut = new StringMap<String>();
+			for (i in values) stringLut.set(i, matchDigits.replace(i, ""));
+			for (j in assign(group, cmp))
+			{
+				j.sort(cmpAlphabetical);
+				tmp.push(j);
+			}
+		}
+		groups = tmp;
+		
+		/* now strings only differ by their numbers; so test if the numbers define a sequence */
+		
+		for (group in groups)
+		{
+			//build lookup tables
+			//example:
+			//foo_bar_5_6_0001
+			//cols    ^ ^ ^
+			//        0 1 2
+			//column 1: left="foo_bar_5_" digits="6" right="_0001"
+			
+			var rows = group.length;
+			var cols = countCols(group[0]);
+			
+			var lut:Array<String> = [];
+			
+			inline function getIndex(col, row) return col * rows + row;
+			
+			var offsetL = cols * rows * 0;
+			inline function setL(col, row, val) lut[offsetL + getIndex(col, row)] = val;
+			inline function getL(col, row) return lut[offsetL + getIndex(col, row)];
+			
+			var offsetN = cols * rows * 1;
+			inline function setN(col, row, val) lut[offsetN + getIndex(col, row)] = val;
+			inline function getN(col, row) return lut[offsetN + getIndex(col, row)];
+			
+			var offsetR = cols * rows * 2;
+			inline function setR(col, row, val) lut[offsetR + getIndex(col, row)] = val;
+			inline function getR(col, row) return lut[offsetR + getIndex(col, row)];
+			
+			var tmp = group.copy();
+			for (col in 0...cols)
+			{
+				for (row in 0...rows)
+				{
+					matchDigits.match(tmp[row]);
+					tmp[row] = matchDigits.matchedRight(); //match from left to right
+					
+					var left = matchDigits.matchedLeft();
+					if (col > 0) left = getL(col - 1, row) + getN(col - 1, row) + left;
+					setL(col, row, left);
+					setN(col, row, matchDigits.matched(0));
+					setR(col, row, matchDigits.matchedRight());
+				}
+			}
+			
+			/* scan from rightmost column (=max) to leftmost column; */
+			
+			var pass;
+			
+			var col = cols;
+			while (--col > -1)
+			{
+				//check if string portions *left of* digits are exactly the same
+				pass = true;
+				var first = getL(col, 0);
+				for (i in 1...rows)
+				{
+					if (first != getL(col, i))
+					{
+						pass = false;
+						break;
+					}
+				}
+				if (!pass) mTrailingDigitsOnly ? break : continue; //no sequence possible
+				
+				//check if string portions *right of* digits are exactly the same
+				if (col < cols - 1)
+				{
+					pass = true;
+					var first = getR(col, 0);
+					for (i in 1...rows)
+					{
+						if (first != getR(col, 1))
+						{
+							pass = false;
+							break;
+						}
+					}
+				}
+				if (!pass) mTrailingDigitsOnly ? break : continue; //no sequence possible
+				
+				//all leading strings match; now check if digits are of same length (padded with leading zeros).
+				
+				var firstLength = getN(col, 0).length;
+				for (i in 1...rows)
+				{
+					if (firstLength != getN(col, i).length)
+					{
+						pass = false;
+						break;
+					}
+				}
+				if (!pass) mTrailingDigitsOnly ? break : continue; //no sequence possible
+				
+				//check if digits define a counter, there might be gaps
+				var sorted = [];
+				var tmp = group.copy();
+				for (i in 0...rows) sorted[Std.parseInt(getN(col, i))] = tmp[i];
+				
+				var name = getL(col, 0) + '{counter}' + getR(col, 0);
+				
+				getSequences(sorted, name, output);
+				
+				break; //found sequence(s); no need to search remaining cols
+			}
+		}
+		return output;
+	}
+	
+	function discardStringsWithoutDigits(inout:Array<String>)
+	{
+		var matchDigits = ~/\d+/g;
+		var i = 0;
+		var k = inout.length;
+		while (i < k)
+		{
+			if (matchDigits.match(inout[i]))
+			{
+				i++;
+				continue;
+			}
+			k--;
+			var last = inout.pop();
+			if (i < k) inout[i] = last;
+		}
+	}
+	
+	function getRegEx(s:String):EReg
+	{
 		var pattern = "^", ereg = ~/\d+/, p;
 		do
 		{
-			if (ereg.match(string))
+			if (ereg.match(s))
 			{
 				p = ereg.matchedPos();
-				pattern += '.{${p.pos}}\\d{${p.len}}';
-				string = ereg.matchedRight();
+				pattern += '\\D{${p.pos}}\\d{${p.len}}';
+				s = ereg.matchedRight();
 			}
 			else
 			{
-				pattern += '.{${string.length}}';
+				pattern += '\\D{${s.length}}';
 				break;
 			}
 		}
@@ -113,130 +221,85 @@ class FileSequence
 		return new EReg(pattern, "");
 	}
 	
-	static function split(input:Array<String>, equal:String->String->Bool):Array<Array<String>>
+	function assign<T>(input:Array<T>, compare:T->T->Bool):Array<Array<T>>
 	{
-		var list = new Sll<String>(input), output = [], set = [], first:String;
-		while (!list.isEmpty())
+		var list = input.copy();
+		var output = new Array<Array<T>>();
+		var a = new Array<T>();
+		var k = list.length, tail;
+		var i, j;
+		while (k > 0)
 		{
-			var n = list.head;
-			
-			first = n.val;
-			set.push(n.val);
-			list.removeHead();
-			
-			n = list.head;
-			while (n != null)
+			tail = list.pop();
+			k--;
+			i = k - 1; j = 0;
+			while (i >= 0)
 			{
-				var hook = n.next;
-				if (equal(first, n.val))
+				if (compare(tail, list[i]))
 				{
-					set.push(n.val);
-					n.unlink();
+					//swap i with last element and pop
+					a[j++] = list[i];
+					list[i] = list[--k];
+					list.pop();
 				}
-				n = hook;
+				i--;
 			}
-			
-			if (set.length > 1)
+			if (j > 0)
 			{
-				output.push(set);
-				set = [];
+				//output group
+				a[j++] = tail;
+				output.push(a);
+				a = [];
 			}
-			else
-				set.pop();
 		}
 		return output;
 	}
 	
-	/**
-		Finds all file sequences in `a`. At this point all strings only differ by their numbers
-	**/
-	static function test(a:Array<String>):Array<Array<String>>
+	function countCols(s:String):Int
 	{
-		var output = [];
-		var ereg = ~/\d+/;
-		
-		/* determine #levels, e.g. "name_1_name001_name2 has 3 levels */
-		
-		var levels = 0;
-		var s = a[0];
-		do
+		var matchDigits = ~/\d+/g;
+		var c = 0;
+		while (true)
 		{
-			if (ereg.match(s))
-			{
-				s = ereg.matchedRight();
-				levels++;
-			}
-			else
-				break;
+			if (!matchDigits.match(s)) break;
+			s = matchDigits.matchedRight();
+			c++;
 		}
-		while (true);
+		return c;
+	}
+	
+	function getSequences(sorted:Array<String>, name:String, output:Array<{name:String, items:Array<String>}>)
+	{
+		var min = -1;
+		var max = -1;
 		
-		/* compare from left -> right */
-		
-		var list = a.copy();
-		
-		inline function match(s:String, numLevels:Int):{pos:Int, right:String}
+		inline function add()
 		{
-			for (i in 0...numLevels)
-			{
-				ereg.match(s);
-				s = ereg.matchedRight();
-			}
-			return {pos: Std.parseInt(ereg.matched(0)), right: s};
+			var sequence = [];
+			while (min <= max) sequence.push(sorted[min++]);
+			if (sequence.length > 1)
+				output.push({name: name, items: sequence});
 		}
 		
-		var level = 1;
-		while (level <= levels)
+		var i = 0;
+		var k = sorted.length;
+		while (i < k)
 		{
-			var remainder = [];
-			
-			while (list.length > 0)
+			if (min == -1 && sorted[i] != null)
 			{
-				//first entry defines string right of number
-				var sequence = [];
-				
-				var o = match(list[0], level);
-				
-				var right = o.right;
-				sequence.push({pos: o.pos, name: list[0]});
-				
-				list.shift();
-				
-				var i = 0;
-				var k = list.length;
-				while (i < k)
-				{
-					var o = match(list[i], level);
-					
-					if (right == o.right)
-					{
-						sequence.push({pos: o.pos, name: list[i]});
-						
-						list.splice(i, 1);
-						k--;
-						continue;
-					}
-					
-					i++;
-				}
-				
-				if (sequence.length == 1)
-				{
-					//no sequence found; remember for next iteration with level + 1
-					remainder.push(sequence[0].name);
-					continue;
-				}
-				
-				//found sequence, sort by position and store
-				sequence.sort(function(a, b) return a.pos - b.pos);
-				output.push([for (i in 0...sequence.length) sequence[i].name]);
+				min = i;
+				max = i;
+				i++;
+				continue;
 			}
-			
-			//process next level
-			level++;
-			list = remainder;
+			if (min != -1 && sorted[i] == null)
+			{
+				add();
+				min = -1;
+			}
+			max++;
+			i++;
 		}
-		
-		return output;
+		if (min != -1) add();
 	}
 }
