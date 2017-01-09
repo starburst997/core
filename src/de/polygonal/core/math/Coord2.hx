d13aed65d0250f08229bc993da9be25b7747f934
Michael Baczynski <michaelbaczynski@gmail.com>
2017-01-09 21:02:06 +0100

    Add readOnly property

--- src/de/polygonal/core/math/Coord2.hx
+++ src/de/polygonal/core/math/Coord2.hx
@@ -32,13 +32,20 @@ class Coord2f implements Coord2<Float>
 	public var x:Float;
 	public var y:Float;
 	
+	#if debug
+	public var readOnly = false;
+	#end
+	
 	public function new(x:Float = 0, y:Float = 0)
 	{
-		set(x, y);
+		this.x = x;
+		this.y = y;
 	}
 	
 	public inline function of<T:Float>(other:Coord2<T>):Coord2f
 	{
+		assert(!readOnly);
+		
 		x = other.x;
 		y = other.y;
 		return this;
@@ -46,6 +53,8 @@ class Coord2f implements Coord2<Float>
 	
 	public inline function set(x:Float, y:Float):Coord2f
 	{
+		assert(!readOnly);
+		
 		this.x = x;
 		this.y = y;
 		return this;
@@ -58,6 +67,8 @@ class Coord2f implements Coord2<Float>
 	
 	public inline function makeZero()
 	{
+		assert(!readOnly);
+		
 		x = y = 0;
 	}
 	
@@ -82,24 +93,31 @@ class Coord2i implements Coord2<Int>
 	public var x:Int;
 	public var y:Int;
 	
+	#if debug
+	public var readOnly = false;
+	#end
+	
 	public function new(x:Int = 0, y:Int = 0)
 	{
-		set(x, y);
+		this.x = x;
+		this.y = y;
 	}
 	
 	public inline function of<T:Float>(other:Coord2<T>):Coord2i
 	{
+		assert(!readOnly);
+		
 		x = Std.int(other.x);
 		y = Std.int(other.y);
-		
 		return this;
 	}
 	
 	public inline function set(x:Int, y:Int):Coord2i
 	{
+		assert(!readOnly);
+		
 		this.x = x;
 		this.y = y;
-		
 		return this;
 	}
 	
@@ -110,6 +128,8 @@ class Coord2i implements Coord2<Int>
 	
 	public inline function makeZero()
 	{
+		assert(!readOnly);
+		
 		x = y = 0;
 	}
 	
